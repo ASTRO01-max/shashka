a66895e10ec04732b93c481f0c32ec5d530de065
ASTRO01-max <mfulsefer@gmail.com>
2025-01-08 15:24:56 +0000

    shashka

--- shashka.py
+++ shashka.py
@@ -1,4 +1,14 @@
 def pawn(x1, y1, x2, y2):
     return y2-y1==1 and abs(x2-x1)==1
 
-print(pawn(x1=1, y1=1, x2=2, y2=3))
+def miniKing(x1, y1, x2, y2, is_damka=False):
+    if is_damka:
+        return abs(x2 - x1) == abs(y2 - y1)
+    else:  
+        return y2 - y1 == 1 and abs(x2 - x1) == 1
+
+
+
+
+
+
